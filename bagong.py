bfedda0e7f77452c6dc7fdfa0c3d5fe6d90f07b9
Aces007 <acelawrencezclavano@iskolarngbayan.pup.edu.ph>
2022-02-14 16:59:50 -0800

    txt working

--- bagong.py
+++ bagong.py
@@ -1,37 +1,33 @@
-from base64 import decode
-import cv2
 import numpy as np
-from pyzbar.pyzbar import decode
 from datetime import datetime
+import cv2
+from pyzbar.pyzbar import decode
 
-# QRKo = cv2.imread('frame.png')
-CamTime = cv2.VideoCapture(0)
-CamTime.set(3,640)
-CamTime.set(4,480)
-
+# ItoQRKo = cv2.imread('frame.png')
+CameraTime = cv2.VideoCapture(0)
+CameraTime.set(3,640)
+CameraTime.set(4,480)
 
 while True:
-    success, QRKo = CamTime.read()
-    
-
-    for codes in decode(QRKo):
+    success, ItoQRKo = CameraTime.read()
+    for codes in decode(ItoQRKo):
         print(codes.data)
         MyTxt = open('Personal.txt', 'w')
-        DecodeData = codes.data.decode('utf-8')
-        print(DecodeData)
-
+        TheDataInside = codes.data.decode('utf-8')
+        print(TheDataInside)
 
         Dimensions = np.array([codes.polygon], np.int32)
         Dimensions = Dimensions.reshape(-1,1,2)
-        cv2.polylines(QRKo,[Dimensions], True, (255,0,255), 5)
 
-
+        cv2.polylines(ItoQRKo,[Dimensions], True, (255,0,255), 5)
         # Record Date
         RealTime = datetime.now()
-        MyTxt.write(RealTime.strftime("Present Date: %B/%d/%Y \n"))
-        MyTxt.write(RealTime.strftime("Current Time: %H:%M:%S"))
-        MyTxt.close()
+        RealTimeOpen = RealTime.strftime("Present Date: %B/%d/%Y \n Current Time: %H:%M:%S")
 
-    cv2.imshow('Result', QRKo)
+        MyTxt.write(f'{TheDataInside} \n {RealTimeOpen}')
+        
+
+
+    cv2.imshow('Result', ItoQRKo)
     cv2.waitKey(1)
 
