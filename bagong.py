738d4cdafdd73042864b6ad2bd501c7c47ae501b
Aces007 <acelawrencezclavano@iskolarngbayan.pup.edu.ph>
2022-02-14 17:06:46 -0800

    All in proper form

--- bagong.py
+++ bagong.py
@@ -11,15 +11,20 @@ CameraTime.set(4,480)
 while True:
     success, ItoQRKo = CameraTime.read()
     for codes in decode(ItoQRKo):
+        
+        #Res
         print(codes.data)
         MyTxt = open('Personal.txt', 'w')
         TheDataInside = codes.data.decode('utf-8')
         print(TheDataInside)
 
+        
+        # BorderLine Authentication
         Dimensions = np.array([codes.polygon], np.int32)
         Dimensions = Dimensions.reshape(-1,1,2)
-
         cv2.polylines(ItoQRKo,[Dimensions], True, (255,0,255), 5)
+        
+        
         # Record Date
         RealTime = datetime.now()
         RealDateOpen = RealTime.strftime("Present Date: %B/%d/%Y \n")
